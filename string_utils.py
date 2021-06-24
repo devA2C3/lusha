b5922f883a8b9d1031ef69a107e9a06559773969
Ilya Kritsmer <ilyakritsmer@Ilyas-Air.Dlink>
2021-06-24 08:57:25 +0300

    remove redundant symbols

--- string_utils.py
+++ string_utils.py
@@ -4,14 +4,3 @@ def slice_right(string, index):
 
 def slice_left(string, index):
     return string[-index:]
-
-def __get_current_substring_suffix(self, current_substring, post_text_pattern_score):
-    return current_substring[-post_text_pattern_score:]
-
-
-def __get_next_substring_prefix(self, current_substring, prefixScore):
-    return current_substring[:prefixScore]
-
-
-def __get_next_prefix(prefixString, prefixScore):
-    return prefixString[-prefixScore:]
